455398683211a7eece4ddb22ff7cbf34112e0d7c
Jérémy <jeremy.vaneste@free.fr>
2022-05-17 18:02:58 +0200

    correction style of setupForm

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import { useState } from "react";
 import SetupForm from "./components/form/SetupForm";
 import { AppContextProvider } from "./context/context";
-
 function App() {
   const [start, setStart] = useState(false);
 
   return (
     <AppContextProvider>
       <div className="container">
-        <SetupForm started={start}></SetupForm>
+        <div
+          style={{
+            height: "100vh",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+          className="container_setup_form"
+        >
+          <SetupForm started={start}></SetupForm>
+        </div>
       </div>
     </AppContextProvider>
   );
