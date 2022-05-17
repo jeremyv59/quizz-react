b8e80299e5bc9f5a1e22e93b2df5979982fc2506
Jérémy <jeremy.vaneste@free.fr>
2022-05-17 18:04:56 +0200

    app.css creation & move div style into

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import { useState } from "react";
 import SetupForm from "./components/form/SetupForm";
 import { AppContextProvider } from "./context/context";
+import "../src/app.css";
+
 function App() {
   const [start, setStart] = useState(false);
 
   return (
     <AppContextProvider>
       <div className="container">
-        <div
-          style={{
-            height: "100vh",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-          }}
-          className="container_setup_form"
-        >
+        <div className="container_setup_form">
           <SetupForm started={start}></SetupForm>
         </div>
       </div>
