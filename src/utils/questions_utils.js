50c9fa20d6e97465112a463f09d9b6815e65438f
jérémy vaneste <jeremy.vaneste@free.fr>
2022-05-19 18:24:53 +0200

    install html-entities to decode

--- src/utils/questions_utils.js
+++ src/utils/questions_utils.js
@@ -24,7 +24,25 @@ export const getAnswers = (contextData) => {
   }
   return finalArr;
 };
-// Fctn to clean question (delete &quot;/)
+
+// Fctn to encode question title 
+// export const encodeToUTF16 = (message) => {
+//   // On procède octet par octet
+//   return message
+//     .split("")
+//     .map((char) => {
+//       // Pour chaque octet, on récupère sont point de code
+//       const word = char.codePointAt(0).toString(16);
+
+//       // Si le point de code ne fait qu'un seul octet, on ajoute des 0
+//       // Ceci permet d'obtenir la longueur fixe de l'UTF-16
+//       if (word.length === 2) return `00${word}`;
+//       return word;
+//     })
+//     .join("");
+// };
+
+// Fctn to clean question (replace &quot;/)
 export const cleanQuestionTitle = (questionTitle) => {
   let finalText = "";
   finalText = questionTitle.replace(/&quot;/g, '\\"');
