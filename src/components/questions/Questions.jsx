87ce41d066371307f42c2d2b7597c5a9b7b805e2
jérémy vaneste <jeremy.vaneste@free.fr>
2022-05-30 14:17:03 +0200

    button to switch on next question ok

--- src/components/questions/Questions.jsx
+++ src/components/questions/Questions.jsx
@@ -2,7 +2,7 @@ import { Button } from "antd";
 import React, { useContext, useEffect, useState } from "react";
 import { AppContext } from "../../context/context";
 import { decode } from "html-entities";
-import { checkBooleanAnswer, getAnswers } from "../../utils/questions_utils";
+import { getAnswers } from "../../utils/questions_utils";
 import { IoArrowForward } from "react-icons/io5";
 import "../questions/questions.css";
 
@@ -20,17 +20,44 @@ const Questions = () => {
   // shuffleArray(demoArray);
 
   const handleClickAnswer = (e) => {
+    console.log("e.target.textContent", e.target);
     setSelectedAnswer(e.target.textContent);
   };
 
   const checkAnswer = () => {
+    console.log(
+      "selectedAnswer === questions[answerCounter].correct_answer",
+      selectedAnswer,
+      questions[answerCounter].correct_answer
+    );
+    // setAnswerCounter(answerCounter++);
     if (selectedAnswer === questions[answerCounter].correct_answer) {
-      setGoodAnswers(goodAnswersCounter++);
+      setGoodAnswers(goodAnswersCounter + 1);
     }
+    setAnswerCounter(answerCounter + 1);
   };
 
-  const handleSubmitAnswer = () => {
-    checkAnswer();
+  const handleSubmitAnswer = async () => {
+    // checkAnswer();
+    console.log(
+      "questions[answerCounter].correct_answer",
+      questions[answerCounter].correct_answer
+    );
+    // setAnswerCounter(answerCounter++);
+
+    try {
+      // if (selectedAnswer === questions[answerCounter].correct_answer) {
+      //   console.log("bonne réponse");
+      //   // setGoodAnswers(goodAnswersCounter++);
+      checkAnswer();
+      // }
+
+      // setAnswerCounter(answerCounter++);
+    } catch (e) {
+      console.log("submit error");
+    }
+
+    // setAnswerCounter(answerCounter++);
   };
 
   useEffect(() => {
@@ -41,10 +68,61 @@ const Questions = () => {
 
   return (
     <div className="container_questions">
-      {context.questions.length > 0 ? (
+      {/* {context.questions.length > 0 ? (
         <div>
           {questions.map((question, index) => {
-            if (index === 0) {
+            if (index === answerCounter) {
+              return (
+                <React.Fragment key={index}>
+                  <h4 className="good_answers_title">{`Réponses correctes : ${goodAnswersCounter}/${index}`}</h4>
+                  <h1 className="question_title">
+                    {decode(
+                      question.question,
+                      "&lt; &gt; &quot; &apos; &amp; &#169; &#8710; &#039;"
+                    )}
+                  </h1>
+         
+                  {answers && answers.length > 0
+                    ? answers[index].map((answer, indexAnswer) => {
+                        return (
+                          <div className="container_buttons">
+                            <Button
+                              key={indexAnswer}
+                              className={
+                                selectedAnswer === answer
+                                  ? "button_item_active"
+                                  : "button_item"
+                              }
+                              onClick={(e) => handleClickAnswer(e)}
+                            >
+                              {answer}
+                            </Button>
+                          </div>
+                        );
+                      })
+                    : "Erreur"}
+                </React.Fragment>
+              );
+            }
+          
+          })}
+          <div className="btn_next_container">
+            <Button
+              onClick={() => handleSubmitAnswer()}
+              className="btn_next_question"
+            >
+              Question suivante
+              <IoArrowForward className="arrow_icon"></IoArrowForward>
+            </Button>
+          </div>
+        </div>
+      ) : (
+        "Chargement..."
+      )} */}
+      {questions.length > 0 ? (
+        <div>
+          {questions.map((question, index) => {
+            if (index === answerCounter) {
               return (
                 <React.Fragment key={index}>
                   <h4 className="good_answers_title">{`Réponses correctes : ${goodAnswersCounter}/${index}`}</h4>
@@ -74,19 +152,22 @@ const Questions = () => {
                         );
                       })
                     : "Erreur"}
+                  <div className="btn_next_container">
+                    <Button
+                      onClick={() => handleSubmitAnswer()}
+                      className="btn_next_question"
+                    >
+                      Question suivante
+                      <IoArrowForward className="arrow_icon"></IoArrowForward>
+                    </Button>
+                  </div>
                 </React.Fragment>
               );
             }
           })}
-          <div className="btn_next_container">
-            <Button onClick={handleSubmitAnswer} className="btn_next_question">
-              Question suivante
-              <IoArrowForward className="arrow_icon"></IoArrowForward>
-            </Button>
-          </div>
         </div>
       ) : (
-        "Chargement..."
+        "Chargement"
       )}
     </div>
   );
